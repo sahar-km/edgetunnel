e8bd0f9149ef7b6a65c33401d4f62f88c040023a
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-04-17 19:41:23 +0330

    Update and rename vless.js to worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-// <!--GAMFC-->version base on commit 841ed4e9ff121dde0ed6a56ae800c2e6c4f66056, time is 2024-04-16 18:02:37 UTC<!--GAMFC-END-->.
+// <!-- time is 2024-04-16 18:02:37 UTC --!>
 // @ts-ignore
 import { connect } from 'cloudflare:sockets';
 
