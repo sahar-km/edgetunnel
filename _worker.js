1a104f9315d5b4b84e64d73ba081cceb1279a1a2
Strawberry <165325276+sahar-km@users.noreply.github.com>
2024-05-18 01:10:11 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = '152.67.9.19';
+let proxyIP = '193.123.81.105';
 
 
 if (!isValidUUID(userID)) {
